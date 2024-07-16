0ad3e5b3a1670ff0eb2792bd4ac6ecdfb58b2408
JustinEvans-github <53385587+JustinEvans-github@users.noreply.github.com>
2024-07-15 21:41:24 -0400

    added readme docs

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# typepixels
+# Keyboard Art Generator
+[![HitCount](https://hits.dwyl.com/justinj-evans/keyboard-art-generator.svg?style=flat)](http://hits.dwyl.com/justinj-evans/keyboard-art-generator)
+
+
+Keyboard Art Generator is a simple and interactive tool that enables you to design and generate art using the keycaps of a keyboard. It transforms your creative concepts into a visual representation made entirely of keyboard keys.
+
+## Demo
+<p align="center">
+    <a href="https://"><img src="./docs/demo.gif" width=700></a>
+</p>
+
+## Running the app locally
+```bash
+git clone https://github.com/justinj-evans/keyboard-art-generator.git
+pip install -r requirements.txt
+streamlit run app.py
+```
+
+## Contributors
+Contributions of any kind welcome.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/justinj-evans/typepixels/blob/0c88a814231349d1c975e71f1dc4949f4398e3bc/LICENSE) file for details.
--- app.py
+++ app.py
@@ -6,44 +6,58 @@ from image import PIL_to_PNG
 # # Streamlit app
 st.title("Keyboard Art Generator")
 
+# Example
+with st.expander('Example'):
+    e1, e2, e3, e4, e5 = st.columns(5,gap='large')
+    with e1: st.image(r"docs\landscape.PNG",width=100, caption='Choose an Image')
+    with e2: st.image(r"docs\inputs.PNG",width=100, caption='Select Inputs')
+    with e3: st.image(r"docs\landscape-keyboard-generator.PNG",width=100, caption='Pixelate Your Image')
+    with e4: st.image(r"docs\keyboard.PNG",width=100, caption='Recycle Keycaps from Keyboard')
+    with e5: st.image(r"docs\landscape-keys.PNG",width=100, caption='Keyboard Artwork')
+
 # Upload image
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+container = st.container(border=True)
+uploaded_file = container.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 # SideBar
 # Dropdowns
-colors = st.sidebar.multiselect("Standard KeyBoard Colors",options=['black','white','grey'],default=[],                             help='Leave blank to default to average color')
-grid_size = st.sidebar.slider("Number of Keys (width)", min_value=10, max_value=100, value=10, step=5)
+st.sidebar.title("Select Inputs")
+colors = st.sidebar.multiselect("Standard Keyboard Colors",options=['black','white','grey'],default=[],                             help='Leave blank to default to average color')
+grid_size = st.sidebar.slider("Keycap Width", min_value=10, max_value=100, value=10, step=5)
 
 # Process Image
-if uploaded_file is not None:
-    pixelater = ImageGridProcessor(image_path=uploaded_file, colors=colors)
-else:
-    image = r'image/python.PNG'
-    pixelater = ImageGridProcessor(image_path=image, colors=colors)
+with st.container(border=True):
 
-pixelated_image = pixelater.process_and_display(grid_size=grid_size)
-pixelater.process_grid_colors()
-grid_color_matrix_count_export = '\n'.join(str(row) for row in pixelater.grid_color_matrix_count)
+    if uploaded_file is not None:
+        pixelater = ImageGridProcessor(image_path=uploaded_file, colors=colors)
+    else:
+        image = r'image/python.PNG'
+        pixelater = ImageGridProcessor(image_path=image, colors=colors)
 
-# Metrics
-m1, m2 = st.columns(2,gap='small')
-with m1: st.metric(label='Keys', value=grid_size*grid_size)
-with m2: st.metric(label='Colors', value=len(pixelater.grid_colors_count))
+    pixelated_image = pixelater.process_and_display(grid_size=grid_size)
+    pixelater.process_grid_colors()
+    grid_color_matrix_count_export = '\n'.join(str(row) for row in pixelater.grid_color_matrix_count)
 
-# Display Grid Color Counts in a row
-color_grids = 10
-cols = st.columns(len(pixelater.grid_colors_count[0:color_grids]), gap='small')
-for i, (color, count) in enumerate(pixelater.grid_colors_count[0:color_grids][0:color_grids]):
-    with cols[i]:
-        image = Image.new('RGB', size=(50, 50), color=color)
-        # Add a black border
-        border_color = (0, 0, 0)
-        border_size = 1  # Change this value to adjust the border size
-        image_with_border = ImageOps.expand(image, border=border_size, fill=border_color)
-        st.image(image_with_border,width=50, caption=f'x {count}')
+    # Metrics
+    m1, m2, m3 = st.columns(3,gap='large')
+    with m1: st.metric(label='Grid Keys', value=grid_size*grid_size)
+    with m2: st.metric(label='Colors', value=len(pixelater.grid_colors_count))
+    with m3: st.metric(label="Width (inches)", value=(grid_size*0.75))
 
-# Display Pixelated Image
-st.image(pixelated_image, caption='Pixelated image', use_column_width=True)
+    # Display Grid Color Counts in a row
+    color_grids = 10
+    cols = st.columns(len(pixelater.grid_colors_count[0:color_grids]), gap='small')
+    for i, (color, count) in enumerate(pixelater.grid_colors_count[0:color_grids][0:color_grids]):
+        with cols[i]:
+            image = Image.new('RGB', size=(50, 50), color=color)
+            # Add a black border
+            border_color = (0, 0, 0)
+            border_size = 1  # Change this value to adjust the border size
+            image_with_border = ImageOps.expand(image, border=border_size, fill=border_color)
+            st.image(image_with_border,width=50, caption=f'x {count}')
+
+    # Display Pixelated Image
+    st.image(pixelated_image, caption='Pixelated image', use_column_width=True)
 
 # Download Button - Export Instructions
 d1, d2 = st.columns(2,gap='small')
